74d3d40b1ad3d01eec54aab5ad79df74e015bef3
Carmi1234 <carmi.adame@hotmail.com>
2020-07-11 02:29:11 +0000

    excuse generator final

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,22 @@ import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
-
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["ate", "pissed", "crushed", "broke"];
+  let when = [
+    "before class",
+    "right on time",
+    "when I finished",
+    "under the car"
+  ];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  document.querySelector("#excuse").innerHTML =
+    randomWho + " " + randomWhat + " " + randomWhen;
+
+  console.log(randomWho + " " + randomWhat + " " + randomWhen);
 };
